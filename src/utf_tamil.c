ce2fef5d795b449225754a1dd92cf6e74e6b7b1d
deepaks <sdeepaks1999@gmail.com>
2024-11-23 22:23:55 +0530

    updates: refactoring and change in implementation approach

--- src/utf_tamil.c
+++ src/utf_tamil.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include "utf_tamil.h"
 
-#define TOTAL_TA_langPOINTS 128
+#define TOTAL_TAMIL_CODEPOINTS 128
 
 /*
  * A Key-Value map:
@@ -24,40 +24,59 @@
  * 14       : aaitham
  * 15 to 28 : First 14 agara uyirmei
  * 29 to 32 : last 4 agara uyirmei
- * 33       : "ja" non-tamil letter
- * 34 to 37 : other 4 non-tamil letters
+ * 33       : "ja" vadamozhi letter
+ * 34 to 37 : other 4 vadamozhi letters
  * 38 to 48 : inaippu letters which makes other uyir mei 
  * 49 to 58 : tamil numbers 
  * 59 to 72 : remaining 
  */
-static const int utf_ta_map[TOTAL_TA_langPOINTS] = {
-    0, 0, 72, 14, 0, 2, 3, 4, 5, 6, 7, 0, 0, 0, 8, 9, 
-    10, 0, 11, 12, 13, 15, 0, 0, 0, 16, 17, 0, 33, 0, 18, 19, 
-    0, 0, 0, 20, 21, 0, 0, 0, 22, 32, 23, 0, 0, 0, 24, 25, 
-    26, 31, 27, 30, 29, 28, 34, 35, 36, 37, 0, 0, 0, 0, 38, 39, 
-    40, 41, 42, 0, 0, 0, 43, 44, 45, 0, 46, 47, 48, 1, 0, 0, 
-    70, 0, 0, 0, 0, 0, 0, 71, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 
-    59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 0, 0, 0, 0, 0
+static const int utf8_tamil_key_map[TOTAL_TAMIL_CODEPOINTS] = {
+    0   ,0  ,72 ,14 ,0  ,2  ,3  ,4  ,5  ,6  ,7  ,0  ,0  ,0  ,8  ,9  
+    ,10 ,0  ,11 ,12 ,13 ,15 ,0  ,0  ,0  ,16 ,17 ,0  ,33 ,0  ,18 ,19 
+    ,0  ,0  ,0  ,20 ,21 ,0  ,0  ,0  ,22 ,32 ,23 ,0  ,0  ,0  ,24 ,25
+    ,26 ,31 ,27 ,30 ,29 ,28 ,34 ,35 ,36 ,37 ,0  ,0  ,0  ,0  ,38 ,39
+    ,40 ,41 ,42 ,0  ,0  ,0  ,43 ,44 ,45 ,0  ,46 ,47 ,48 ,1  ,0  ,0
+    ,70 ,0  ,0  ,0  ,0  ,0  ,0  ,71 ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0
+    ,0  ,0  ,0  ,0  ,0  ,0  ,49 ,50 ,51 ,52 ,53 ,54 ,55 ,56 ,57 ,58
+    ,59 ,60 ,61 ,62 ,63 ,64 ,65 ,66 ,67 ,68 ,69 ,0  ,0  ,0  ,0  ,0
 };
 
-static int utf_8_cp_rank(const char *str, int utf_8_type, UTF8_Lang lang);
+typedef int (*CompareFunction)(const char *first, const char *second);
 
+static int compare_default_cp(const char *first, const char *second);
+static int compare_tamil_cp(const char *first, const char *second);
+
+typedef struct {
+    UTF8_Lang       language;
+    UTF8_Type       type;
+    CompareFunction fn_compare;
+} UTF8_LanguageSet; 
+
+/*
+ * **WARNING**: The below order should follow the UTF8_Lang Type 
+ * If any modification in future: remember the above 
+ * Note: This is implemented because of future modification/additions in mind 
+ */
+UTF8_LanguageSet lang_object_map[UTF8_LANG_TOTAL] = {
+    {UTF8_LANG_TAMIL    ,UTF8_TYPE_3        ,compare_tamil_cp},
+    {UTF8_LANG_ENGLISH  ,UTF8_TYPE_1        ,compare_default_cp},
+    {UTF8_LANG_UNKNOWN  ,UTF8_TYPE_UNKNOWN  ,compare_default_cp}
+};
 
 /*
  * Function to get the type of the First UTF-8 code point in a string pointer 
  */
-int utf_8_type(const char *str, UTF8_Lang *lang) 
+int get_utf8_info(const char *str, UTF8_Lang *lang) 
 {
     if (str == NULL) 
-        return -1;
+        return UTF8_TYPE_UNKNOWN;
 
-    if (lang != NULL) *lang = LANG_UNKNOWN;
+    if (lang != NULL) *lang = UTF8_LANG_UNKNOWN;
     
     if ((str[0] & 0x80) == 0x00) {
-        if (lang != NULL && (str[0] >= 'a' && str[0] <= 'z')
-        && (str[0] >= 'A' && str[0] <= 'Z')) {
-            *lang = LANG_ENGLISH;
+        /* Set corresponding Language of this type */
+        if (lang != NULL) {
+            *lang = UTF8_LANG_ENGLISH;
         } 
         return UTF8_TYPE_1;
     }
@@ -69,9 +88,9 @@ int utf_8_type(const char *str, UTF8_Lang *lang)
     if (((str[0] & 0xF0) == 0xE0) && ((str[1] & 0xC0) == 0x80) 
     && ((str[2] & 0xC0) == 0x80)) {
         if (lang != NULL) {
-            /* To check if the codepoint is in TAMIL Language */
+            /* Set corresponding Language of this type: only tamil can be found now */
             if (str[0] == -32 && (str[1] == -82 || str[1] == -81)) {
-                *lang = LANG_TAMIL;
+                *lang = UTF8_LANG_TAMIL;
             } 
         } 
         return UTF8_TYPE_3;
@@ -88,17 +107,16 @@ int utf_8_type(const char *str, UTF8_Lang *lang)
 /*
  * Function to count the UTF-8 code points in a string 
  */
-int utf_8_cp_count(const char *str) 
+int get_utf8_cp_count(const char *str) 
 {
     int index = 0;
     int count = 0;
     int jump;
 
-    if (str == NULL) 
-        return -1;
+    if (str == NULL) return -1;
 
     while(str[index] != '\0') {
-        jump = utf_8_type(&str[index], NULL);
+        jump = get_utf8_info(&str[index], NULL);
 
         if (jump == 0) jump = 1;
 
@@ -110,110 +128,122 @@ int utf_8_cp_count(const char *str)
 }
 
 /*
- * Function to compare TAMIL string in UTF-8 format 
- * Note: Works best if the string is entirely tamil 
- */
-int utf_8_ta_compare(const char *first, const char *second) 
+ * Function to compare string in UTF-8 format (most suitable for Tamil) 
+ * Note: Currently this works best for TAMIL language, hence this name 
+ */ 
+int utf8_compare_tamil(const char *first, const char *second) 
 {
-    int f_index = 0, s_index = 0;
+    int f_index, s_index;
     int f_jump, s_jump;
-    int f_key, s_key;
+    int comparison;
+
     UTF8_Lang f_lang, s_lang;
 
-    if (first == NULL && second == NULL) 
-        return 0;
+    if (first == NULL && second == NULL) return 0;
+    if (first == NULL) return -1;
+    if (second == NULL) return 1;
 
-    if (first == NULL) 
-        return 1;
-
-    if (second == NULL) 
-        return -1;
+    f_index = 0;
+    s_index = 0;
 
     while (first[f_index] != '\0' && second[s_index] != '\0') {
-        f_jump = utf_8_type(&first[f_index], &f_lang);
-        s_jump = utf_8_type(&second[s_index], &s_lang);
+        f_jump = get_utf8_info(&first[f_index], &f_lang);
+        s_jump = get_utf8_info(&second[s_index], &s_lang);
 
-        if (f_jump == 0) f_jump = 1;
-        if (s_jump == 0) s_jump = 1;
+        if (f_jump == UTF8_TYPE_UNKNOWN) f_jump = 1;
+        if (s_jump == UTF8_TYPE_UNKNOWN) s_jump = 1;
 
-        if (f_lang != LANG_TAMIL && s_lang != LANG_TAMIL) {
-            f_key = utf_8_cp_rank(&first[f_index], f_jump, f_lang);
-            s_key = utf_8_cp_rank(&second[s_index], s_jump, s_lang);
-
-            if (f_key != s_key) {
-                return f_key - s_key;
-            }
-
-            f_index += f_jump;
-            s_index += s_jump;
-            continue;
+        if (f_lang != s_lang) {
+            return f_lang - s_lang;
         }
 
-        if (f_lang != LANG_TAMIL) return 1;
-        if (s_lang != LANG_TAMIL) return -1;
-
-        /*
-         * Logic to get the key of each Tamil codepoints 
-         * used to get its map-value for comparison using utf_ta_map array 
-         */
-        f_key = ((first[f_index + 1] & 1) * 64) + (128 + first[f_index + 2]);
-        s_key = ((second[s_index + 1] & 1) * 64) + (128 + second[s_index + 2]);
-
-        if (utf_ta_map[f_key] != utf_ta_map[s_key]) 
-            return (utf_ta_map[f_key] - utf_ta_map[s_key]);
+        comparison = lang_object_map[f_lang].fn_compare(&first[f_index], &second[s_index]);
+        if (comparison != 0) {
+            return comparison;
+        }
 
         f_index += f_jump;
         s_index += s_jump;
     }
 
-    if (first[f_index] != '\0') { 
-        utf_8_type(&first[f_index], &f_lang);
-        if (f_lang == LANG_TAMIL && first[f_index + 2] == -115) {
-            return -1;
-        }
-        return 1;
-    }
-
-    if (second[s_index] != '\0') {
-        utf_8_type(&second[s_index], &s_lang);
-        if (s_lang == LANG_TAMIL && second[s_index + 2] == -115) {
-            return 1;
-        }
-        return -1;
-    }
+    if (first[f_index] != '\0') return 1;
+    if (second[s_index] != '\0') return -1;
 
     return 0;
 } 
 
 /*
- * Function to get the rank of the NON-TAMIL codepoints in UTF-8
- * if rank is lower, then the letter has lexicographically lower order
- * else, otherwise 
- * 
- * we form the rank by just multiplying the 'unsigned char' value of 
- * each byte in a codepoint 
- * Note: This is applicable only for 'NON-TAMIL' codepoints 
- */
-static int utf_8_cp_rank(const char *str, int utf_8_type, UTF8_Lang lang) 
+ * Function to compare the initial codepoints in default way!
+ * **BEWARE**: When this function is being called, that means 
+ * 1. UTF8_Type of initial codepoints of first and second string should be equal 
+ */ 
+static int compare_default_cp(const char *first, const char *second) 
 {
-    int rank = 1;
+    UTF8_Type utf8_type;
     int index;
 
-    if (str == NULL) {
-        return -1;
+    utf8_type = get_utf8_info(first, NULL);
+
+    if (utf8_type == UTF8_TYPE_UNKNOWN) {
+        return (first[0] - second[0]);
     }
 
-    if (utf_8_type == 1) {
-        if (lang == LANG_ENGLISH && str[0] >= 'a' && str[0] <= 'z') {
-            return (str[0] - 32);
+    if (utf8_type == UTF8_TYPE_1) {
+        return (first[0] - second[0]);
+    }
+
+    for (index = 0; index < utf8_type; index++) {
+        if (first[index] != second[index]) {
+            return (first[index] - second[index]);
         }
-        return ((str[0] & 0x80) ? -1 : str[0]);
     }
 
-    for (index = 0; index < utf_8_type; index++) {
-        rank *= (unsigned char) str[index];
+    return 0;
+}
+
+/*
+ * Function to compare the initial codepoints of the TAMIL strings 
+ * **BEWARE**: If this function is being called, that means
+ * 1. The Language of initial codepoints of first and second strings should be TAMIL 
+ */
+static int compare_tamil_cp(const char *first, const char *second) 
+{
+    int f_key, s_key;
+    int f_rank, s_rank;
+    UTF8_Lang f_lang_next, s_lang_next;
+
+    f_key = ((first[1] & 1) * 64) + (128 + first[2]);
+    s_key = ((second[1] & 1) * 64) + (128 + second[2]);
+
+    f_rank = utf8_tamil_key_map[f_key];
+    s_rank = utf8_tamil_key_map[s_key];
+    
+    if (f_rank == s_rank) {
+        /*
+         * The below scenario to handle 
+         * comparison between 'agara uyir mei' and its own 'mei',
+         * when the next codepoint of the word with 'agara uyir mei' is "Not-Tamil"
+         * in this case, the word which is having 'mei' should comes first in order 
+         * Example: first = "பட" (ப+ட+'\0') vs second = "பட்" (ப+ட+்+'\0')
+         * in the above example, '\0' is a non-tamil code point (it can be any non-tamil cp)
+         * Expectation: compare(first, second), returns 1
+         * If we don't handle it here, it will cause the different output (opposite one) 
+         */
+        if (f_rank >= 15 && f_rank <= 37) {
+            get_utf8_info(&first[3], &f_lang_next);
+            get_utf8_info(&second[3], &s_lang_next);
+            if (s_lang_next != UTF8_LANG_TAMIL && f_lang_next == UTF8_LANG_TAMIL) {
+                if (first[5] == -115) {
+                    return -1;
+                }
+            }
+            if (f_lang_next != UTF8_LANG_TAMIL && s_lang_next == UTF8_LANG_TAMIL) {
+                if (second[5] == -115) {
+                    return 1;
+                }
+            }
+        } 
     }
 
-    return rank;
+    return (f_rank - s_rank);
 } 
-types 
